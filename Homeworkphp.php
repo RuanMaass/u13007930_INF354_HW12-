f14dd19d448ca03bad9799545245dc126950a72b
Ruhan21 <ruhanodendaal06@gmail.com>
2015-05-21 09:52:06 +0200

    Added table and echo name

--- Homeworkphp.php
+++ Homeworkphp.php
@@ -1,4 +1,32 @@
-<?php
+<!DOCTYPE html>
+<HTML>
+  <head>
+    <title>Homework 12 INF 354</title>
+  </head>
+  <body>
+    <tb>
+      <tr>
+        <td><H1>The Team</H1></td>
+      </tr>
+  <tr>
+    <td>
+    <?php
 
 echo "Helllooooo my name is Ruan Maass 13007930";
+
 ?>
+</td>
+    <td>
+      <?php
+
+echo "What up , The name is Ruhan Odendaal 13042514";
+
+?>
+    </td>
+    <td></td>
+  </tr>
+</tb>
+  </body>
+    
+</HTML>
+
